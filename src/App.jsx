a454c7ae3134c78887ebb3483dfb73d529eb3f48
B.Manikanta Aniketh <manikantabondugula@gmail.com>
2026-03-11 20:08:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import "./App.css"
+import Nav from './component/Nav/Nav'
+import Home from './component/Home/Home'
+import Login from './component/Login/login'
+import Signup from './component/Signup/Signup'
+import { Outlet } from 'react-router-dom'
+import Houses from './component/Houses/Houses'
+import Listing from './component/Listing/Listing'
+import Contact from './component/Contactus/Contact'
+import Footer from './component/Footer/Footer'
+import Rooms from './component/Rooms/Rooms'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app-wrapper">
+      <Nav />
+      <main className="main-content">
+        <Outlet />
+
+      </main>
+      <Footer />
+    </div>
   )
 }
 
 export default App
+
